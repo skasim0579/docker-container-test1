fe0aea87216edf610b9fcd36015ba247abbbfb26
abdulkadir <skasim0579@gmail.com>
2020-05-04 09:48:34 -0400

    updated

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $PATH
 export PATH=$PATH:/home/abdulkadir/apache-jmeter-5.2.1/bin
+echo $PATH
 jmeter -n -t ./cst/cst2.jmx -l /jmeter-report/cst/cst2.jtl -JrunNumber=1 -Jproject_folder=$PWD
 
